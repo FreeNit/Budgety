023386bf6142351f9f9acedf1e3f5feb86771b8c
FreeNit <bolilui.dmytro@gmail.com>
2019-03-21 15:56:04 +0200

    Deleting an Item from the UI

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Budgety - 21.03.2019 
-# - Deleting an Item from Our Budget Controller
+# - Deleting an Item from the UI
--- app.js
+++ app.js
@@ -163,6 +163,11 @@ var UIController = (function () {
 
         },
 
+        deleteListItem: function (selectorID) { 
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+        },
+
         clearField: function(){
             var fields, fieldsArr;
 
@@ -265,9 +270,10 @@ var controller = (function(budgetCtrl, UICtrl){
             budgetCtrl.deleteItem(type, ID);
 
             // 2. Delete the item from the UI
+            UICtrl.deleteListItem(itemID);
 
             // 3. Update and show the new budget
-
+            updateBudget();
         }
 
     };
